78d1a2c6f6ecf69a9b01f8dc123b27616d82e3ea
Micha Kops <micha.kops@gmail.com>
2016-04-25 21:33:22 +0200

    Simulation refactored.

--- src/test/scala/com/hascode/tutorial/RecordedSimulation.scala
+++ src/test/scala/com/hascode/tutorial/RecordedSimulation.scala
@@ -8,32 +8,42 @@ import io.gatling.jdbc.Predef._
 
 class RecordedSimulation extends Simulation {
 
-	val httpProtocol = http
-		.baseURL("http://localhost:8080")
-		.inferHtmlResources(BlackList(""".*\.css""", """.*\.js""", """.*\.ico""", """.*\.png""", """.*\.gif"""), WhiteList())
-		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-		.acceptEncodingHeader("gzip, deflate")
-		.acceptLanguageHeader("en-US")
-		.userAgentHeader("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0")
+  val httpProtocol = http
+    .baseURL("http://localhost:8080")
+    .inferHtmlResources(BlackList(""".*\.css""", """.*\.js""", """.*\.ico""", """.*\.png""", """.*\.gif"""), WhiteList())
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-US")
+    .userAgentHeader("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0")
 
-	val headers_0 = Map("REMOTE_USER" -> "admin")
+  val headers_0 = Map("REMOTE_USER" -> "admin")
 
-    val uri1 = "http://localhost:8080/hascode"
+  val uri1 = "http://localhost:8080/hascode"
 
-	val scn = scenario("RecordedSimulation")
-		.exec(http("request_0")
-			.get("/hascode")
-			.headers(headers_0))
-		.pause(25)
-		.exec(http("request_1")
-			.post("/hascode/j_security_check")
-			.headers(headers_0)
-			.formParam("j_username", "admin")
-			.formParam("j_password", "test"))
-		.pause(3)
-		.exec(http("request_2")
-			.get("/hascode/rs/session")
-			.headers(headers_0))
+  object HomePage {
+    val home = exec(http("StartPage")
+      .get("/hascode")
+      .headers(headers_0))
+      .pause(25)
+  }
 
-	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  object LoginPage {
+    val login = exec(http("LoginPage")
+      .post("/hascode/j_security_check")
+      .headers(headers_0)
+      .formParam("j_username", "admin")
+      .formParam("j_password", "test"))
+      .pause(3)
+  }
+
+  object UserInfoPage {
+    val info = exec(http("UserInformation")
+      .get("/hascode/rs/session")
+      .headers(headers_0))
+  }
+
+  val scn = scenario("JavaMultiuserScenario")
+    .exec(HomePage.home, LoginPage.login, UserInfoPage.info)
+
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
